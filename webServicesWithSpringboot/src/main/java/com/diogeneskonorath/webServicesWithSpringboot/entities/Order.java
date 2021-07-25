fe3f30c79d3b343301dc5474d78fb9055a9c2bdf
Diogenes Konorath <diogenesks@gmail.com>
2021-07-25 13:20:10 -0300

    add order status

--- webServicesWithSpringboot/src/main/java/com/diogeneskonorath/webServicesWithSpringboot/entities/Order.java
+++ webServicesWithSpringboot/src/main/java/com/diogeneskonorath/webServicesWithSpringboot/entities/Order.java
@@ -14,6 +14,7 @@ public class Order implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -21,9 +22,10 @@ public class Order implements Serializable {
 
     public  Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -49,6 +51,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
