861d0b7dd62b30917d4f841ec4dd559e7faa23e8
Diogenes Konorath <diogenesks@gmail.com>
2021-07-28 18:27:43 -0300

    add order items and its configurations

--- webServicesWithSpringboot/src/main/java/com/diogeneskonorath/webServicesWithSpringboot/config/TestConfig.java
+++ webServicesWithSpringboot/src/main/java/com/diogeneskonorath/webServicesWithSpringboot/config/TestConfig.java
@@ -2,10 +2,7 @@ package com.diogeneskonorath.webServicesWithSpringboot.config;
 
 import com.diogeneskonorath.webServicesWithSpringboot.entities.*;
 
-import com.diogeneskonorath.webServicesWithSpringboot.repositories.CategoryRepository;
-import com.diogeneskonorath.webServicesWithSpringboot.repositories.OrderRepository;
-import com.diogeneskonorath.webServicesWithSpringboot.repositories.ProductRepository;
-import com.diogeneskonorath.webServicesWithSpringboot.repositories.UserRepository;
+import com.diogeneskonorath.webServicesWithSpringboot.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -29,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -63,5 +63,12 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
--- webServicesWithSpringboot/src/main/java/com/diogeneskonorath/webServicesWithSpringboot/entities/Order.java
+++ webServicesWithSpringboot/src/main/java/com/diogeneskonorath/webServicesWithSpringboot/entities/Order.java
@@ -3,7 +3,9 @@ package com.diogeneskonorath.webServicesWithSpringboot.entities;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -19,6 +21,9 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public  Order(){}
 
@@ -61,6 +66,10 @@ public class Order implements Serializable {
         if (orderStatus != null) {
             this.orderStatus = orderStatus.getCode();
         }
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
